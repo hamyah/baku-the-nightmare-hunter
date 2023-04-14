c7ecc0feace3cd366a63f06d2500d8a4666556c7
Paulo <pauliroti@gmail.com>
2023-04-14 21:40:09 +0100

    removed broken code

--- Assets/Scripts/SpawnTimerManager.cs
+++ Assets/Scripts/SpawnTimerManager.cs
@@ -5,15 +5,5 @@ using UnityEngine.Events;
 
 public class SpawnTimerManager : MonoBehaviour
 {
-    public static UnityEvent onSpawnObject;
 
-    public class SpawnObjectEvent : UnityEvent<int, > {
-        
-    }
-
-    void Start() {
-        if(onSpawnObject == null) {
-            onSpawnObject
-        }
-    }
 }
