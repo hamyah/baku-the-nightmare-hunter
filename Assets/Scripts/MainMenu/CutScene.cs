2367aac8050d2a70e70faf2530a9a8fc44beab19
Paulo <pauliroti@gmail.com>
2023-04-16 14:32:59 +0100

    fixed click issue; fixed video webgl

--- Assets/Scripts/MainMenu/CutScene.cs
+++ Assets/Scripts/MainMenu/CutScene.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class CutScene : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
 
     void Start() {
         LeanTween.scaleZ(gameObject, 1, 35).setOnComplete(Skip);
+
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"Intro.mp4");
+        videoPlayer.Play();
     }
 
     public void Skip() {
