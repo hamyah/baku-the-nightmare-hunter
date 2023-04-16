208b9b2767482edfd0d9dfad32ed65dd4f171b4d
Paulina <paulinajuzwin@gmail.com>
2023-04-16 11:41:56 +0100

    Fix webGL fullscreen. Change pause to "P" instead of "Esc"

--- Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -13,7 +13,7 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
             ShowPauseScreen();
     }
 
